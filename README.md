e8152c43ddd013c4a1856418b487d751496cb8b3
raman <raman@mail.com>
2024-03-19 15:16:48 +0530

    langchn

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Langchain-Tutorials
+
+source myenv/Scripts/activate
+  505  streamlit.exe run example.py
--- example1.py
+++ example1.py
@@ -4,11 +4,7 @@ from constants import openai_key
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
-
-from langchain.memory import ConversationBufferMemory
-
 from langchain.chains import SequentialChain
-
 import streamlit as st
 
 os.environ["OPENAI_API_KEY"]=openai_key
@@ -25,33 +21,26 @@ first_input_prompt=PromptTemplate(
     template="Tell me about celebrity {name}"
 )
 
-# Memory
-
-person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_history')
-dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
-descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
-
 ## OPENAI LLMS
 llm=OpenAI(temperature=0.8)
 chain=LLMChain(
-    llm=llm,prompt=first_input_prompt,verbose=True,output_key='person',memory=person_memory)
+    llm=llm,prompt=first_input_prompt,verbose=True,output_key='person')
 
 # Prompt Templates
-
 second_input_prompt=PromptTemplate(
     input_variables=['person'],
     template="when was {person} born"
 )
 
 chain2=LLMChain(
-    llm=llm,prompt=second_input_prompt,verbose=True,output_key='dob',memory=dob_memory)
-# Prompt Templates
+    llm=llm,prompt=second_input_prompt,verbose=True,output_key='dob')
 
+# Prompt Templates
 third_input_prompt=PromptTemplate(
     input_variables=['dob'],
     template="Mention 5 major events happened around {dob} in the world"
 )
-chain3=LLMChain(llm=llm,prompt=third_input_prompt,verbose=True,output_key='description',memory=descr_memory)
+chain3=LLMChain(llm=llm,prompt=third_input_prompt,verbose=True,output_key='description')
 parent_chain=SequentialChain(
     chains=[chain,chain2,chain3],input_variables=['name'],output_variables=['person','dob','description'],verbose=True)
 
@@ -59,9 +48,3 @@ parent_chain=SequentialChain(
 
 if input_text:
     st.write(parent_chain({'name':input_text}))
-
-    with st.expander('Person Name'): 
-        st.info(person_memory.buffer)
-
-    with st.expander('Major Events'): 
-        st.info(descr_memory.buffer)
